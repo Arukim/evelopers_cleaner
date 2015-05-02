733419e8e2f7c66880bad94fe0e0e1c9900165d3
Arukim <smelovn@gmail.com>
2015-05-02 23:45:41 +0300

    first version

--- main.c
+++ main.c
@@ -1,25 +1,80 @@
 #include <stdio.h>
 
 //Test data
-char * test_in[] = {"//c-style comment \\n",
-		      "//c-style comment A\\n//c-style comment B",
-		      "/*c++ style comment}*/\\n",
+#define TESTS_COUNT 5
+#define MAX_STRING 255
+char test_in[TESTS_COUNT][MAX_STRING] = {"//c-style comment \n",
+		      "//c-style comment A\n//c-style comment B",
+		      "/*c++ style comment}*/\n",
 		      "No comments at all",
-		      "/*C++ // c comment*/\n"};
-char * test_out[] = {"                 \\n",
-		      "                   \\n                   ",
-		      "                      \\n",
+		      "/*C++ // c comment*//* very hard * comment */\n"};
+char test_out[TESTS_COUNT][MAX_STRING] = {"                  \n",
+		      "                   \n                   ",
+		      "                      \n",
 		      "No comments at all",
-		      "                    \n"};     
+		      "                                             \n"};     
+
+typedef enum CommentState_{
+  None,
+  SlashFound,
+  CStyle,
+  CppStyle,
+  CppStyleEnd
+}CommentState;
 
 void remove_Comments(char *psz_str){
-  
+  CommentState commentState = None;
+  do{
+    switch(commentState){
+    case None:
+      if(*psz_str == '/'){
+	commentState = SlashFound;
+      }
+      break;
+    case SlashFound:
+      if(*psz_str == '/'){
+	commentState = CStyle;
+	*psz_str = ' ';
+	*(psz_str - 1) = ' ';
+      }else if(*psz_str == '*'){
+	commentState = CppStyle;
+	*psz_str = ' ';
+	*(psz_str - 1) = ' ';
+      }else{
+	commentState = None;
+      }
+      break;
+    case CStyle:
+      if(*psz_str == '\n'){
+	commentState = None;
+      }else{
+	*psz_str = ' ';
+      }
+      break;      
+    case CppStyle:
+      if(*psz_str == '*'){
+	commentState = CppStyleEnd;
+      }
+      *psz_str = ' ';
+      break;
+    case CppStyleEnd:
+      if(*psz_str == '/'){
+	commentState = None;
+      }else{
+	commentState = CppStyle;
+      }
+      *psz_str = ' ';
+      break;
+    default:
+      break;
+    }
+  }while(*++psz_str);
 }
 
 
 int main() {
   int i;
-  for(i=0; i<sizeof(test_in); i++){
+  for(i=0;i< TESTS_COUNT; i++){
     remove_Comments(test_in[i]);
     if(strcmp(test_in[i], test_out[i])==0){
       printf("Test %d passed\n", i);
