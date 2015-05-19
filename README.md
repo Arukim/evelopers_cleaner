621cc3158f5cad5a6ef3ffebc936dbefe56358aa
Arukim <smelovn@gmail.com>
2015-05-19 20:05:35 +0300

    Moved output write to separate func, added some comments

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # evelopers_cleaner
 
-Comments are replaced with whitespaces
-
 Only Unix EOL style is supported
 
 Comments inside string literals are not ignored
--- main.c
+++ main.c
@@ -2,21 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define FILESYSTEM_BUF_SIZE 4096
+#define FILESYSTEM_BUF_SIZE 4
 
-/*Test data*/
-#define TESTS_COUNT 5
-#define MAX_STRING 255
-char test_in[TESTS_COUNT][MAX_STRING] = {"//cpp-style comment \n",
-		      "//cpp-style comment A\n//cpp-style comment B",
-		      "/*c style comment}*/\n",
-		      "No comments at all",
-		      "/*c // cpp\n comment*/\n/* very hard * comment */\n"};
-char test_out[TESTS_COUNT][MAX_STRING] = {"\n",
-		      "\n",
-		      "\n",
-		      "No comments at all",
-		      "\n\n"};     
+typedef struct OutputBuffer_{
+  char buf[FILESYSTEM_BUF_SIZE];
+  int pos;
+  FILE * file;
+}OutputBuffer;
 
 typedef enum CommentState_{
   None,
@@ -26,8 +18,29 @@ typedef enum CommentState_{
   CStyleEnd
 }CommentState;
 
+void init_Output(FILE * file, OutputBuffer * buf){
+  buf->file = file;
+  buf->pos = 0;
+}
+
+void write_Output(char ch, OutputBuffer * buf){
+  /** TODO: add input check*/
+  if(!(buf->pos < sizeof(buf->buf))){
+    fwrite(buf->buf, 1, sizeof(buf->buf), buf->file);
+	buf->pos = 0;
+  }
+  buf->buf[buf->pos] = ch;
+  buf->pos++;
+}
+
+void flush_Output(OutputBuffer * buf){
+  fwrite(buf->buf, 1, buf->pos, buf->file);
+  buf->pos = 0;
+}
+
 /*replace C/Cpp style comments with selected symbol */
 void remove_Comments(char *psz_file_in, char *psz_file_out){
+
   FILE * pFile_In = fopen(psz_file_in, "r");
   if(pFile_In == NULL){
     return;
@@ -39,72 +52,87 @@ void remove_Comments(char *psz_file_in, char *psz_file_out){
     return;
   }
 
-  char buf[FILESYSTEM_BUF_SIZE];
-  int len = fread(buf, 1, FILESYSTEM_BUF_SIZE, pFile_In);
+  char buf_In[FILESYSTEM_BUF_SIZE];
+  OutputBuffer output_Buffer;
+  init_Output(pFile_Out, &output_Buffer);
+
+  /**Read first chunk from input */
+  int len = fread(buf_In, 1, sizeof(buf_In), pFile_In);
   CommentState commentState = None;
 
   while(len > 0){
 
     int i = 0;
     for(i = 0; i < len; i++){
+      
       switch(commentState){
       case None:
-	if(buf[i] == '/'){
+	if(buf_In[i] == '/'){
 	  commentState = SlashFound;
-	  continue;
+	  continue;/**move to next character without write operation*/
 	}
 	break;
       case SlashFound:
-	if(buf[i] == '/'){
+	if(buf_In[i] == '/'){
 	  commentState = CppStyle;
-	  continue;
-	}else if(buf[i] == '*'){
+	  continue; /** move on*/
+	}else if(buf_In[i] == '*'){
 	  commentState = CStyle;
-	  continue;
+	  continue; /** move on*/
 	}else{
 	  commentState = None;
-	  fputc(buf[i-1], pFile_Out);
+	  /** False comment start was found, we should write prev '/' */
+	  write_Output(buf_In[i-1], &output_Buffer);
 	}
 	break;
       case CppStyle:
-	if(buf[i] == '\n'){
+	if(buf_In[i] == '\n'){
 	  commentState = None;
 	}else{
-	  continue;
+	  continue; /** move on */
 	}
 	break;      
       case CStyle:
-	if(buf[i] == '*'){
+	if(buf_In[i] == '*'){
 	  commentState = CStyleEnd;
 	}
-	continue;
+	continue; /** move on */
 	break;
       case CStyleEnd:
-	if(buf[i] == '/'){
+	if(buf_In[i] == '/'){
 	  commentState = None;
 	}else{
 	  commentState = CStyle;
 	}
-	continue;
+	continue; /** move on */
 	break;
       default:
 	break;
       }
-    
-      fputc(buf[i], pFile_Out);
+
+      /**At this point symbol is sent to output*/
+      write_Output(buf_In[i], &output_Buffer);
     }
 
-    if(commentState == SlashFound){
-      buf[0] = '/';
-      len = 1 + fread(buf + 1, 1, FILESYSTEM_BUF_SIZE - 1, pFile_In);
+    /**In_Buf is empty, need to pull next chunk*/
+    if(commentState != SlashFound){
+      /**Usually just get next chunk from file */
+       len = fread(buf_In, 1, sizeof(buf_In), pFile_In);
+    }else{
+      /** If we are in SlashFound state, we need to last symbol
+       * of prev buffer*/
+      buf_In[0] = '/';
+      len = 1 + fread(buf_In + 1, 1, sizeof(buf_In) - 1, pFile_In);
       if(len == 1){
-	fputc('/', pFile_Out);
+	/**file suddenly ended, write down last symbol */
+	write_Output('/', &output_Buffer);
 	break;
       }
-    }else{
-      len = fread(buf, 1, FILESYSTEM_BUF_SIZE, pFile_In);
     }
   }
+
+  /** Flush temp output buffer to file */
+  flush_Output(&output_Buffer);
   
   fclose(pFile_In);
   fclose(pFile_Out);
